283ef0d7a6858aadba8122dd6443710b260cd8ce
WiseEagle <wiseeaglesc@gmail.com>
2021-12-18 21:16:53 +0200

    added first blocks, styles and broken down into components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="app-wrapper-content">
+            <div>
+                <div className="profile-header">
+                    <img src="https://www.partyrama.co.uk/wp-content/uploads/2014/02/beach006.jpg" alt=""/>
+                </div>
+                <div className="description-block">
+                    <img src="https://nulm.gov.in/images/user.png"/> + desc
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
