cbb963f805ac2e27f051e2597dd511ca18692d22
WiseEagle <wiseeaglesc@gmail.com>
2022-08-14 18:42:08 +0300

    ***Video 20 Lesson 20, add Navlinks to Navbar for disable reload pages when we going to pages

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import logo from "../../logo.png";
 import React from "react";
 import s from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <header className={s.header}>
-            <a href="/"><img src={logo} alt=""/></a>
+            <NavLink to="/"><img src={logo} alt=""/></NavLink>
         </header>
     );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import s from "./Navbar.module.css";
-import {Route} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Dialogs from "../Dialogs/Dialogs";
 import Profile from "../Profile/Profile";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div><a href="/profile"  className={s.item}>Profile</a></div>
-            <div><a href="/dialogs"  className={s.item}>Messages</a></div>
-            <div><a href="/news"  className={s.item}>News</a></div>
-            <div><a href="/news"  className={s.item}>Music</a></div>
-            <div><a href="/settings"  className={s.item}>Settings</a></div>
+            <div><NavLink to="/profile"  className={({ isActive }) => isActive ? `${s.item} ${s.active}` : s.item} >Profile</NavLink></div>
+            <div><NavLink to="/dialogs"  className={({ isActive }) => isActive ? `${s.item} ${s.active}` : s.item}>Messages</NavLink></div>
+            <div><NavLink to="/news"  className={({ isActive }) => isActive ? `${s.item} ${s.active}` : s.item}>News</NavLink></div>
+            <div><NavLink to="/music"  className={({ isActive }) => isActive ? `${s.item} ${s.active}` : s.item}>Music</NavLink></div>
+            <div><NavLink to="/settings"  className={({ isActive }) => isActive ? `${s.item} ${s.active}` : s.item}>Settings</NavLink></div>
         </nav>
     );
 }
